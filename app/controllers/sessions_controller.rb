dbb71d876bb3f257c5147897682f26f4d7c1f63c
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-28 21:51:52 -0400

    Creating logged_in method in sessions controller and also creating a concern for us to check the current user that is logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
 #SessionsController is used for signing the user in.
+include CurrentUserConcern
     # Post request 
     def create
         user = User.find_by(email: params["user"]["email"]).try(:authenticate, params["user"]["password"])
@@ -17,6 +18,21 @@ class SessionsController < ApplicationController
                 status: 401
             }
         end 
+    end 
+
+
+    # checks to see if a current user is available
+    def logged_in
+        if @current_user
+            render json:{
+                logged_in: true, 
+                user: @current_user
+            }
+        else 
+            render json: {
+                logged_in: false
+            }
+        end
     end 
 
 end 
