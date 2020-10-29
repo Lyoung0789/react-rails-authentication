84cfd76dfd4bd28cb79d004f9a86f5c201615dbe
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-28 21:58:48 -0400

    Adding some logic in the session_store so that we can access the api in development mode

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
 # domain is where the source will be where the working api will be at
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ""
+if Rails.env == "production "
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: ""
+else 
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end 
