cd0b69109762599cd15cabb9acecd6100bf591af
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-28 21:54:27 -0400

    Creating the logout method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,6 +24,7 @@ include CurrentUserConcern
     # checks to see if a current user is available
     def logged_in
         if @current_user
+            #this does not return a http status because this is a get request not a post. 
             render json:{
                 logged_in: true, 
                 user: @current_user
@@ -33,6 +34,15 @@ include CurrentUserConcern
                 logged_in: false
             }
         end
+    end 
+
+    #logs us out of the api
+    def logout
+        reset_session 
+        render json: {
+            status: 200, 
+            logged_out: true
+        }
     end 
 
 end 
